3d7b67bfc3babde012d2d4b7ea16c0445a769ee2
andehleh <76177056+andehleh@users.noreply.github.com>
2026-03-04 17:26:39 -0500

    Update firebase.js

--- ultra-heatmap/lib/firebase.js
+++ ultra-heatmap/lib/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDr6O1zSOLXK83EfpykWLhie0Ia0Bdg2Ig",
   authDomain: "heat-map-76125.firebaseapp.com",
@@ -14,9 +9,8 @@ const firebaseConfig = {
   storageBucket: "heat-map-76125.firebasestorage.app",
   messagingSenderId: "358564471566",
   appId: "1:358564471566:web:fd418d6a9ce1c6b8f3ad5f",
-  measurementId: "G-1Z808C9QP6"
+  measurementId: "G-1Z808C9QP6",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getDatabase(app);
